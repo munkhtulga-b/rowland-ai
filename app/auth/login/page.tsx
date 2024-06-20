d665d378bd6585ff5871ce6daca7a66399ce259e
munkhtulga-mirai <munkhtulga@mirai.mn>
2024-06-20 14:40:35 +0800

    merge

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -8,6 +8,7 @@ import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { toast } from "react-toastify";
 import Cookies from "js-cookie";
+import { generateUUID } from "@/app/_utils/helpers";
 
 const accounts: TypeLoginAccount[] = [
   {
@@ -49,27 +50,6 @@ const LoginPage = () => {
       setIsLoading(false);
     }
   };
-
-  function generateUUID() {
-    let d = new Date().getTime();
-    let d2 = (performance && performance.now && performance.now() * 1000) || 0; // Time in microseconds since page-load or 0 if unsupported
-    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(
-      /[xy]/g,
-      function (c) {
-        let r = Math.random() * 16; // random number between 0 and 16
-        if (d > 0) {
-          // Use timestamp until depleted
-          r = (d + r) % 16 | 0;
-          d = Math.floor(d / 16);
-        } else {
-          // Use microseconds since page-load if supported
-          r = (d2 + r) % 16 | 0;
-          d2 = Math.floor(d2 / 16);
-        }
-        return (c === "x" ? r : (r & 0x3) | 0x8).toString(16);
-      }
-    );
-  }
 
   return (
     <>
