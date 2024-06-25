e9b4b602ec446d479faaefa97fb72d044086f5b6
amkamirai <amartaivan@mirai.mn>
2024-06-25 18:14:19 +0800

    merge fix

--- app/_api/index.ts
+++ app/_api/index.ts
@@ -2,6 +2,10 @@
 import login from "@/app/_api/auth/LoginController";
 import register from "@/app/_api/auth/SignupController";
 import verify from "@/app/_api/auth/VerifyEmailController";
+import forgot from "@/app/_api/auth/ForgotPasswordController"
+import reset from "@/app/_api/auth/ResetPasswordController"
+
+import * as chat from "@/app/_api/chat/ChatHistoryController"
 
 const $api = {
   auth: {
@@ -13,9 +17,6 @@ const $api = {
   },
   user: {
     chat,
-  },
-  admin: {
-    exportCSV
   },
 };
 
