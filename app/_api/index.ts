f3ee0303b305e43c62332c8c15abba93ae1f2806
amkamirai <amartaivan@mirai.mn>
2024-06-25 18:08:29 +0800

    merge

--- app/_api/index.ts
+++ app/_api/index.ts
@@ -2,7 +2,6 @@
 import login from "@/app/_api/auth/LoginController";
 import register from "@/app/_api/auth/SignupController";
 import verify from "@/app/_api/auth/VerifyEmailController";
-import exportCSV from "@/app/_api/admin/ExportCSVController";
 
 const $api = {
   auth: {
